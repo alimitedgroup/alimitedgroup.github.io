28eec33c9d37052bc6f6e091b4e8ca6c3a67d1da
esposm03 <36164633+esposm03@users.noreply.github.com>
2024-10-21 18:59:14 +0200

    New website

---
+++ .github/workflows/website.py
@@ -0,0 +1,32 @@
+# Script for updating website with new documents
+
+TEMPLATE = '<li><a href="{{link}}">{{name}}</a></li>'
+
+from glob import glob
+from pathlib import Path
+from shutil import rmtree, copyfile, copytree
+
+# Setup `dist` directory
+rmtree('dist', ignore_errors=True)
+copytree('website', 'dist', symlinks=False)
+
+verbali_interni = []
+
+# Iterate over all PDF files, copy them into `dist`,
+# and save the paths, to later insert them into the html
+for file in glob('**/*.pdf', recursive=True):
+    if 'verbali/interni' in file:
+        verbali_interni.append(file)
+        Path('dist/verbali/interni').mkdir(parents=True, exist_ok=True)
+        copyfile(file, 'dist/' + file)
+    else:
+        print('ERROR: unhandled file ' + file)
+        exit(1)
+
+# update the html file
+html = Path('dist/index.html').read_text()
+html = html.replace('{{verbali_interni}}', '\n'.join(
+    TEMPLATE.replace('{{link}}', file).replace('{{name}}', file.split('.')[-2].split('/')[-1])
+    for file in verbali_interni
+))
+Path('dist/index.html').write_text(html)
--- README.md
+++ README.md
@@ -14,3 +14,13 @@ il comando da eseguire (dalla cartella radice della repository) è:
 ```
 typst compile --root . path/to/document.typ
 ```
+
+## Sito
+
+Questa repository include un sito web. Per avviarlo da locale, eseguire:
+
+- `python3 .github/workflows/compile.py` per compilare i PDF
+- `python3 .github/workflows/website.py` per generare il sito dentro alla cartella `dist/`
+- `cd dist && python3 -m http.server` per avviare un server HTTP
+
+Dopo questi comandi, il sito sarà disponibile su http://localhost:8000
