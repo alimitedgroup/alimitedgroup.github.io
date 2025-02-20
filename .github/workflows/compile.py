3ed15c3f989291169ec6427f2607816e375952ce
esposm03 <36164633+esposm03@users.noreply.github.com>
2025-02-20 11:20:52 +0100

    Aggiunta versione del glossario nel sito

--- .github/workflows/compile.py
+++ .github/workflows/compile.py
@@ -106,6 +106,7 @@ def loadGlossary() -> dict:
 def process_template(titolo: str) -> str:
     titolo = titolo.strip()
     nomefile = titolo + ".pdf"
+    if 'Glossario' in nomefile: nomefile = 'Glossario.pdf'
 
     if "PQ " in titolo:
         titolo = titolo.replace('PQ ', 'Piano di Qualifica ')
--- lib/glossary.typ
+++ lib/glossary.typ
@@ -9,7 +9,7 @@
   redattori: (),
   body,
 ) = {
-  [#metadata[Glossario] <titolo>]
+  [#metadata[Glossario #versione] <titolo>]
   set text(lang: "it", font: "Hanken Grotesk")
   prima-pagina(
     [_Glossario_ \ Versione #versione],
