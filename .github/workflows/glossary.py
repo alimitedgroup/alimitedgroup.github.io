b36213d0945c4b8710ba8c3955df9168dfe45fb0
mpiccoli18 <mpiccoli18@gmail.com>
2025-04-03 17:32:09 +0200

    Fix if

--- .github/workflows/glossary.py
+++ .github/workflows/glossary.py
@@ -62,7 +62,7 @@ def substitute(filePath,glossaryYml):
                 bodyFound=True
             if bodyFound==True and ")" in line:
                 parFound=True
-        elif len(line.strip()) > 0 and line.lstrip()[0] == '#' and ("/03-PB\\manuale-utente" in filePath or "\\03-PB\\manuale-utente"):
+        elif len(line.strip()) > 0 and line.lstrip()[0] == '#' and ("/03-PB\\manuale-utente" in filePath or "\\03-PB\\manuale-utente" in filePath):
             newText += line 
             line=file.readline()
             logging.error(f'Found # as the start of the line --> linebreak')
